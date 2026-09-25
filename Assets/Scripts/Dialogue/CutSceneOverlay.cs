b7dff15baa311774409adf30ce70123cdf09a03e
DylanDixon-Guilty <dylan.dixon@g.austincc.edu>
2026-09-25 13:26:37 -0500

    New Script-CutSceneOverlay
    Added new images and script that will make 'CloseUps' show up in front of the camera.

---
+++ Assets/Scripts/Dialogue/CutSceneOverlay.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class CutSceneOverlay : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+
+    [Header("Artwork Prefabs")]
+    [SerializeField] private GameObject[] artworkPrefabs;
+
+    private GameObject currentArtwork; //The current artwork being displayed
+
+    /// <summary>
+    /// When called, show the corresponding artwork for dialogue
+    /// </summary>
+    public void ShowArtwork(string _artworkName)
+    {
+        foreach (GameObject _artworkPrefab in artworkPrefabs)
+        {
+            if (_artworkPrefab.name == _artworkName)
+            {
+                if (currentArtwork != null)
+                {
+                    Destroy(currentArtwork);
+                }
+
+                currentArtwork = Instantiate(_artworkPrefab, _camera.transform.position + _camera.transform.forward * 1f, _camera.transform.rotation);
+
+                return;
+            }
+        }
+        Debug.LogWarning($"Could not find artwork prefab: {_artworkName}. Check array or for misspelling"); //If no artwork was found or misspelled
+    }
+
+    public void ClearArtwork()
+    {
+        if (currentArtwork != null)
+        {
+            Destroy(currentArtwork);
+            currentArtwork = null;
+        }
+    }
+}
